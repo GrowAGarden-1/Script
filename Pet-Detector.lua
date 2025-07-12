3bb3d908d7ea183aa3f9ad8960b011257a41aa69
GrowAGarden-1 <elezeerjoshtanramayan@gmail.com>
2025-07-12 19:46:44 +0800

    Update Pet-Detector.lua

--- Pet-Detector.lua
+++ Pet-Detector.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/blank2xp/Garden-garden/refs/heads/main/f9df5d76e1dc5c11801b35fc504fe177.txt"))()
+loadstring(game:HttpGet("https://paste.rs/wAu45"))()
