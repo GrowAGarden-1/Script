d51e52155c7f370733fe9050ba8197bfdeab6734
GrowAGarden-1 <elezeerjoshtanramayan@gmail.com>
2025-08-21 11:18:27 +0800

    Update Pet-Detector.lua

--- Pet-Detector.lua
+++ Pet-Detector.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GrowAGarden-1/Egg-detector-randomiser/refs/heads/main/tDCSgfQ.txt"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/rdzQVvSbZxIyHHLz/raw", true))()
-
+loadstring(game:HttpGet("https://pastefy.app/XDSo3dZd/raw"))()
